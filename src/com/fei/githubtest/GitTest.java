b3b8aeba01665abb6225fe6ac1364b1bf82e491b
feifeiheleilei <https://github.com/feifeiheleilei/Thinking-in-Java.git>
2015-06-08 10:54:06 +0800

    06/08/2015-2

--- src/com/fei/githubtest/GitTest.java
+++ src/com/fei/githubtest/GitTest.java
@@ -1,10 +1,16 @@
 package com.fei.githubtest;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class GitTest {
 
+	public void speak(){
+		System.out.print("github");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GitTest gitTest = new GitTest();
+		gitTest.speak();
 	}
 
 }
